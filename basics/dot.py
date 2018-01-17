6fa8ed936c30f09ea3d9a357d583588ab0d8bbe3
chq-matteo <chq.matteo@gmail.com>
2018-01-17 11:25:52 +0100

    Add assertion error message for clarity

--- basics/dot.py
+++ basics/dot.py
@@ -1,5 +1,12 @@
 import numpy
 from numba import cuda, vectorize, float32
+
+DIFFERENT_LENGTH_MESSAGE = '''
+The length of the vectors differ
+len(A) = {}
+len(B) = {}
+'''
+
 @cuda.reduce
 def sum(a, b):
     return a + b
@@ -9,7 +16,7 @@ def product(a, b):
     return a * b
 
 def dot(A, B):
-    assert(len(A) == len(B))
+    assert len(A) == len(B), DIFFERENT_LENGTH_MESSAGE.format(len(A), len(B))
     C = numpy.zeros(len(A), dtype=numpy.float32)
     C = product(A, B)
     return sum(C)
@@ -17,5 +24,5 @@ def dot(A, B):
 if __name__ == '__main__':
     N = 10 ** 6
     A = numpy.ones(N, dtype=numpy.float32)
-    B = numpy.ones(N, dtype=numpy.float32)
+    B = numpy.ones(N - 1, dtype=numpy.float32)
     print('Expected {} got {}'.format(N, dot(A, B)))
